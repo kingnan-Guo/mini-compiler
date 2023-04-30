2e217c0220461a6f9fc0fd424b629b0668314ff1
kingnan <373734199@qq.com>
2023-04-30 23:45:43 +0800

    codegen 对 遍历 ast 树 将对应的节点转换成 代码

---
+++ src/__test/codegen.spec.ts
@@ -0,0 +1,124 @@
+import { test, expect } from "vitest";
+import { NodeTypes } from "../ast";
+import { codegen } from "../codegen";
+// 遍历 ast 树 将对应的节点转换成 代码
+test("codegen", () => {
+  const ast = {
+    type: NodeTypes.Program,
+    body: [
+      {
+        type: "ExpressionStatement",
+        expression: {
+          type: "CallExpression",
+          callee: {
+            type: "Identifier",
+            name: "add",
+          },
+          arguments: [
+            {
+              type: "NumberLiteral",
+              value: "2",
+            },
+            {
+              type: "CallExpression",
+              callee: {
+                type: "Identifier",
+                name: "subtract",
+              },
+              arguments: [
+                {
+                  type: "NumberLiteral",
+                  value: "4",
+                },
+                {
+                  type: "NumberLiteral",
+                  value: "2",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    ],
+  };
+
+  expect(codegen(ast)).toMatchInlineSnapshot('"add(2, subtract(4, 2));"');
+});
+
+test("two ExpressionStatement", () => {
+  const ast = {
+    type: NodeTypes.Program,
+    body: [
+      {
+        type: "ExpressionStatement",
+        expression: {
+          type: "CallExpression",
+          callee: {
+            type: "Identifier",
+            name: "add",
+          },
+          arguments: [
+            {
+              type: "NumberLiteral",
+              value: "2",
+            },
+            {
+              type: "CallExpression",
+              callee: {
+                type: "Identifier",
+                name: "subtract",
+              },
+              arguments: [
+                {
+                  type: "NumberLiteral",
+                  value: "4",
+                },
+                {
+                  type: "NumberLiteral",
+                  value: "2",
+                },
+              ],
+            },
+          ],
+        },
+      },
+      {
+        type: "ExpressionStatement",
+        expression: {
+          type: "CallExpression",
+          callee: {
+            type: "Identifier",
+            name: "add",
+          },
+          arguments: [
+            {
+              type: "NumberLiteral",
+              value: "2",
+            },
+            {
+              type: "CallExpression",
+              callee: {
+                type: "Identifier",
+                name: "subtract",
+              },
+              arguments: [
+                {
+                  type: "NumberLiteral",
+                  value: "4",
+                },
+                {
+                  type: "NumberLiteral",
+                  value: "2",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    ],
+  };
+
+  expect(codegen(ast)).toMatchInlineSnapshot(
+    '"add(2, subtract(4, 2));add(2, subtract(4, 2));"'
+  );
+});
