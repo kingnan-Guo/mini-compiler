dcc0eedef93ad32cc37c0aa2efb9c378f1da3d38
kingnan <373734199@qq.com>
2023-04-30 22:56:55 +0800

    transformer 的目的是 进一步处理 ast 树

---
+++ src/transformer.ts
@@ -0,0 +1,71 @@
+import { RootNode, NodeTypes } from "./ast";
+import { traverser } from "./traverser";
+
+export function transformer(ast: RootNode) {
+    const newAst = {
+        type: NodeTypes.Program,
+        body: [],
+    }
+
+    ast.context = newAst.body
+
+    traverser(ast, {
+        CallExpression:{
+            enter(node, parent){
+                console.log(node);
+                
+                if (node.type === NodeTypes.CallExpression) {
+
+                    let expression:any =  {
+                        type: "CallExpression",
+                        callee: {
+                          type: "Identifier",
+                          name: node.name,
+                        },
+                        arguments: [],
+                    }
+
+
+
+
+                    
+                    node.context = expression.arguments
+
+                    if (parent?.type !== NodeTypes.CallExpression) {
+                        expression = {
+                            type: "ExpressionStatement",
+                            expression: expression,
+
+                        }
+                    }
+                    parent?.context?.push(expression)
+
+
+                    
+
+                    
+
+
+                }
+            }
+        },
+        NumberLiteral:{
+            enter(node, parent){
+                if (node.type === NodeTypes.NumberLiteral) {
+                    const numberNode:any = {
+                        type:"NumberLiteral",
+                        value: node.value
+                    }
+                    parent?.context?.push(numberNode)
+                }
+            }
+        }
+
+
+    })
+
+    return newAst;
+
+
+
+}
